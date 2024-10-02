8bbd613d644f884f349ec126fcc8253ac366dd37
maxseryogin <mserjohin@odessa-gymnasium8.ukr.education>
2024-10-02 18:40:23 +0300

    добавил пока что главную страничку, логирование с созданного суперюзера и тд.

--- booking_project/urls.py
+++ booking_project/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for booking_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
+from booking_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Главная страница
+    path('my_bookings/', views.my_bookings, name='my_bookings'),  # Страница "Мои бронирования"
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # Страница логина
+    path('book_spot/<int:spot_id>/', views.book_spot, name='book_spot'),  # Бронирование парковочного места
 ]
