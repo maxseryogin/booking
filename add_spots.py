b3f611d25dff6bd69c08dd684e305d9cbcf84826
maxseryogin <mserjohin@odessa-gymnasium8.ukr.education>
2024-10-03 19:10:43 +0300

    добавил парковочные места, которые можно забронировать, отменить и продлить

---
+++ add_spots.py
@@ -0,0 +1,20 @@
+import os
+import django
+from datetime import datetime, timedelta
+
+# Установите настройки Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_project.settings')
+django.setup()
+
+from booking_app.models import ParkingSpot, Location
+
+# Получите существующую локацию или создайте новую
+location, created = Location.objects.get_or_create(name='Main Location')
+
+# Добавьте 24 парковочных места
+for i in range(1, 25):
+    # Установите время, до которого место свободно (например, на 7 дней вперед)
+    free_until = datetime.now() + timedelta(days=1)
+    ParkingSpot.objects.create(number=f'Место {i}', location=location, is_free=True)
+
+print("24 парковочных места добавлены в базу данных.")
